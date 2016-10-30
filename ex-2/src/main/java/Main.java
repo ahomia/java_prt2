db1ed3823258acc9f4d7fb7a791ebc89483e5dc8
ahomia <marisa_mam@mail.ru>
2016-10-31 00:56:28 +0300

    my changes

---
+++ ex-2/src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ahomia on 31.10.2016.
+ */
+public class Main {
+    public static void main(String args[]) {
+        Point p1 = new Point(0, 2);
+        Point p2 = new Point(0, 0);
+        /*
+        System.out.println(Distance.distance(p1,p2));
+         */
+        System.out.println(p1.distance(p2));
+    }
+
+}
--- sandbox/src/main/java/ru/rtqa/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/rtqa/sandbox/MyProgram.java
@@ -1,3 +1,5 @@
+package ru.rtqa.sandbox;
+
 public class MyProgram {
     public static void main(String args[]) {
         System.out.println("Hello!");
