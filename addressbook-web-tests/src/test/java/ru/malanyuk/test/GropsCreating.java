bd56a0aba653c1fd9597084e1e1d381a296962d0
ahomia <marisa_mam@mail.ru>
2016-11-11 06:00:38 +0300

    ex-3

---
+++ addressbook-web-tests/src/test/java/ru/malanyuk/test/GropsCreating.java
@@ -0,0 +1,86 @@
+package ru.malanyuk.test;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class GropsCreating {
+    FirefoxDriver wd;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testGropsCreating() {
+
+        goToPage();
+        initGroupCreating();
+        fillGroupForm(new GroupDate("marina", "malanyuk", "m1"));
+        submitGroupCreating();
+        returnGroupPage();
+    }
+
+    private void returnGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreating() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupDate groupDate) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupDate.getGroupName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
+        
+    }
+
+    private void initGroupCreating() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void goToPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/malanyuk/test/NewContactCreating.java
@@ -0,0 +1,94 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import ru.malanyuk.test.ContactData;
+
+public class NewContactCreating {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.id("LoginForm")).click();
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void NewContactCreating() {
+
+        initAddContact();
+        fillContactForm(new ContactData("Marina", "Malaniuk", "Ahomia", "Artezio", "89873862557", "marina.malaniuk@gmail.com", 3, 2, "1992"));
+        sumbitContactCreating();
+    }
+
+    private void sumbitContactCreating() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+        wd.findElement(By.name("work")).click();
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option["+ contactData.getBithdayDay() +"]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[1]//option["+ contactData.getBithdayDay() +"]")).click();
+        }
+        if (!wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option["+ contactData.getBithdayMounth() +"]")).isSelected()) {
+            wd.findElement(By.xpath("//div[@id='content']/form/select[2]//option["+ contactData.getBithdayMounth() +"]")).click();
+        }
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(contactData.getBithdayYear());
+    }
+
+    private void initAddContact() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
